517cf1d6d72a7ae7fcf485d16f19eb1e7d11613a
Olga <117331957+hap-py-python@users.noreply.github.com>
2026-02-22 10:33:49 -0500

    main.gd modified
    Increased attempts and update UI

--- SpaceHunter_game/Main.gd
+++ SpaceHunter_game/Main.gd
@@ -4,14 +4,14 @@ export(PackedScene) var GoodDrop
 export(PackedScene) var BadDrop
 export(float) var bad_chance = 0.15
 var score = 0
-var life = 5
+var life = 7
 var game_over = false
 
 
 func _ready():
 	randomize()
 	$Menu/Score.text = "Score: 0"
-	$Menu/Life.text = "Life: 5"
+	$Menu/Life.text = "Attempts: 7"
 	$Menu/Message.hide()
 
 func _on_CrystalTimer_timeout():
@@ -26,12 +26,12 @@ func _on_CrystalTimer_timeout():
 	add_child(drop)
 	drop.add_to_group("drops")
 
-	# í ½í´½ SAFE SPAWN (works with circle OR rectangle shapes)
+	#SAFE SPAWN (works with circle and rectangle shapes)
 
 	var cs = drop.get_node("CollisionShape2D")
 	var shape = cs.shape
 
-	var half_w = 16.0  # fallback default
+	var half_w = 16.0  
 
 	if shape is RectangleShape2D:
 		half_w = shape.extents.x * drop.scale.x
@@ -65,14 +65,13 @@ func _on_Catcher_body_entered(body):
 	if game_over:
 		return
 
-	# If it's a "good" drop (positive points), missing it costs a life.
+	# If it's a "good" drop (positive points), missing it costs an attempt.
 	# If it's a bad drop (negative points), missing it does NOT cost a life.
 	if body.has_method("get") and body.get("points") != null:
 		if body.points > 0:
 			life -= 1
 			$Menu/Life.text = "Life: " + str(life)
 	else:
-		# Fallback: if something without "points" hits the catcher, treat as good
 		life -= 1
 		$Menu/Life.text = "Life: " + str(life)
 
